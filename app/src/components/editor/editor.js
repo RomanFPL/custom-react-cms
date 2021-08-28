9a1bd0dcb0ee46cc964f80ebaad9726d389bbc57
Roman Franko <roma.franko99@gmail.com>
2021-08-28 22:01:24 +0300

    Edit text was added to heper

--- app/src/components/editor/editor.js
+++ app/src/components/editor/editor.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, {Component} from "react";
 import "../../helper/iframeLoader.js";
+import DOMHelper from "../../helper/dom-helper.js";
 
 export default class Editor extends Component {
     constructor() {
@@ -26,13 +27,13 @@ export default class Editor extends Component {
         this.currentPage = page;
         axios
             .get(`../${page}?rnd=${Math.random()}`)
-            .then(res => this.parseStrToDOM(res.data))
-            .then(this.wrapTextNodes)
+            .then(res => DOMHelper.parseStrToDOM(res.data))
+            .then(DOMHelper.wrapTextNodes)
             .then(dom => {
                 this.virtualDom = dom;
                 return dom;
             })
-            .then(this.serializeDOMToString)
+            .then(DOMHelper.serializeDOMToString)
             .then(html => axios.post("./api/save_temp_page.php", {html}))
             .then(() => this.iframe.load("../temp.html"))
             .then(() => this.enableEditing());
@@ -40,8 +41,8 @@ export default class Editor extends Component {
 
     save() {
         const newDom = this.virtualDom.cloneNode(this.virtualDom);
-        this.unwrapTextNodes(newDom);
-        const html = this.serializeDOMToString(newDom);
+        DOMHelper.unwrapTextNodes(newDom);
+        const html = DOMHelper.serializeDOMToString(newDom);
         axios
             .post("./api/save_page.php", {pageName: this.currentPage, html});
     }
@@ -58,49 +59,6 @@ export default class Editor extends Component {
     onTextEdit(element){
         const id = element.getAttribute("nodeid");
         this.virtualDom.body.querySelector(`[nodeid="${id}"]`).innerHTML = element.innerHTML;
-    }
-
-    parseStrToDOM = (str) => {
-        const parser = new DOMParser();
-        return parser.parseFromString(str, "text/html"); 
-    }
-
-    wrapTextNodes = dom => {
-        const body = dom.body;
-        let textNodes = [];
-
-        function recursy(element) {
-            element.childNodes.forEach(node => {
-                
-                if(node.nodeName === "#text" && node.nodeValue.replace(/\s+/g, "").length > 0) {
-                    textNodes.push(node);
-                } else {
-                    recursy(node);
-                }
-            })
-        };
-
-        recursy(body);
-
-        textNodes.forEach((node, i) => {
-            const wrapper = dom.createElement('text-editor');
-            node.parentNode.replaceChild(wrapper, node);
-            wrapper.appendChild(node);
-            wrapper.setAttribute("nodeid", i)
-        });
-
-        return dom;
-    }
-    
-    serializeDOMToString = dom => {
-        const serializer = new XMLSerializer();
-        return serializer.serializeToString(dom);
-    }
-
-    unwrapTextNodes = dom => {
-        dom.body.querySelectorAll("text-editore").forEach(element => {
-            element.parentNode.replaceChild(element.firstChild, element);
-        })
     }
 
     loadPageList = () => {
