7915ac3e78573cdd7d32577d036576f2abb8513c
Roman Franko <roma.franko99@gmail.com>
2021-09-08 19:24:37 +0300

    Check dir for img

--- app/api/upload_image.php
+++ app/api/upload_image.php
@@ -5,7 +5,9 @@
         $fileExt= explode("/",  $_FILES["image"]["type"])[1];
         $fileName = uniqid() . "." . $fileExt;
 
-
+        if(!is_dir("../../img/")){
+            mkdir("../../img/");
+        }
         move_uploaded_file($loaded_img, "../../img/" . $fileName);
         echo json_encode( array("src" => $fileName));
     }
